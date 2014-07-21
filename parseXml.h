0bb4e5f02fc678cef6024aebbefa712ae74c5736
zew88 <zewaldo88@gmail.com>
2014-07-21 14:13:55 +0900

    Update parseXml.h

--- parseXml.h
+++ parseXml.h
@@ -6,13 +6,11 @@ class ParseInput
 public :
 
 
-	
-	void
-	parseInputXml(string fileName,char& TypeOfConnection, string& tcp_cc, string& udp_bw, string& delay,bool& ModeOperation,double& errRate, int& jitter, double& alpha,double& k,double& tetha,int& ErrorModel, string& user_bw, string& server_bw, int& htmlSize, string& tcp_mem_user, string& tcp_mem_server);
-	
-	string
-	GetLowerCase(string stringName);
+void
+parseInputXml(string fileName,char& TypeOfConnection, string& tcp_cc, string& udp_bw, string& delay,int& SimuTime,bool& ModeOperation,double& errRate, int& jitter, double& alpha,double& k,double& tetha,int& ErrorModel, string& user_bw, string& server_bw, int& htmlSize, string& tcp_mem_user, string& tcp_mem_server);
+
+string
+GetLowerCase(string stringName);
 
 };
 }
-
